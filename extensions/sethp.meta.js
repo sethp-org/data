700da39e7056bad10f7731a05f1bad2f7dbf27d5
Nikita <nikitadiscore@gmail.com>
2023-08-08 18:33:22 +0300

    project: update

--- extensions/sethp.meta.js
+++ extensions/sethp.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name SetHP Script
 // @description Скрипт для удобной работы с логами
-// @version 1.0.25
+// @version 1.0.26
 // @homepage https://sethp.xyz
 // @match https://arizonarp.logsparser.info/*
 // @match https://forum.arizona-rp.com/*
